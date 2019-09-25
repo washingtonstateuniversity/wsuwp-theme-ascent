9940b214cb7a31f0e95c5d7c17070bb4dcf08d43
Nic Ford <nicdford@gmail.com>
2019-09-25 13:51:34 -0700

    Remove custom post types in favor of utilizing existing plugin to manage data

--- functions.php
+++ functions.php
@@ -14,8 +14,6 @@ require_once 'classes/class-wsuwp-lodge-child.php';
 /**
  * Actions
  */
-add_action( 'init', 'WSU_WP_Lodge_Child::register_project_post_type' );
-add_action( 'add_meta_boxes', 'WSU_WP_Lodge_Child::register_project_meta_box' );
 add_action( 'wp_enqueue_scripts', 'WSU_WP_Lodge_Child::enqueue_scripts' );
 
 /**
