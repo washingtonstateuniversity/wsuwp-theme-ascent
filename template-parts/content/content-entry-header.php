23e53a93b35613f18f1ccbee579ea05c0b92b18c
Nic Ford <nicdford@gmail.com>
2019-10-18 16:12:39 -0700

    Create fallback image for posts w/o featured image

--- template-parts/content/content-entry-header.php
+++ template-parts/content/content-entry-header.php
@@ -12,7 +12,11 @@
 
 <header class="entry-header">
 	<div class="entry-header-featured-image">
-		<?php the_post_thumbnail( 'full' ); ?>
+		<?php if ( empty( get_the_post_thumbnail() ) ) : ?>
+			<img src="https://s3.wp.wsu.edu/uploads/sites/192/2015/04/Clouds-Color0.jpg" alt="Image of clouds">
+		<?php else : ?>
+			<?php the_post_thumbnail( 'full' ); ?>
+		<?php endif; ?>
 	</div>
 
 	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
