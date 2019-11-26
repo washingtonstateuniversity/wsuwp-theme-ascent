6e503fe5fc23e9c0c67c5ec498016696997d34dd
Nic Ford <nicdford@gmail.com>
2019-11-26 15:19:40 -0800

    Fix logic check for empty field values

--- template-parts/archive/archive-header.php
+++ template-parts/archive/archive-header.php
@@ -21,15 +21,16 @@
 		<?php $pod = pods('wsuwp_uc_topics', $tax_object->term_id); ?>
 		<?php $featured_image_display = $pod->display( 'featured_image' ); ?>
 		<?php $featured_image_field = $pod->field( 'featured_image' ); ?>
-		<div class="entry-header-featured-image">
-			<img src="<?php echo $featured_image_display; ?>" alt="<?php echo $featured_image_field['post_title']; ?>">
-		</div>
 
-	<?php else : ?>
-
-		<div class="entry-header-featured-image">
-			<img src="https://s3.wp.wsu.edu/uploads/sites/192/2019/10/brady-bellini-_hpk_92Crhs-unsplash.jpg" alt="white clouds above silhouette of clouds at day">
-		</div>
+		<?php if ($featured_image_field || $featured_image_display != false ) : ?>
+			<div class="entry-header-featured-image">
+				<img src="<?php echo $featured_image_display; ?>" alt="<?php echo $featured_image_field['post_title']; ?>">
+			</div>
+		<?php else : ?>
+			<div class="entry-header-featured-image">
+				<img src="https://s3.wp.wsu.edu/uploads/sites/192/2019/10/brady-bellini-_hpk_92Crhs-unsplash.jpg" alt="white clouds above silhouette of clouds at day">
+			</div>
+		<?php endif; ?>
 
 	<?php endif; ?>
 
