7f03055a6d0e1bf273a1644527e13dec9af8cdba
Nic Ford <nicdford@gmail.com>
2019-11-26 15:11:10 -0800

    Setup template to check for pods featured image field on taxonomy before falling back to blue sky

--- template-parts/archive/archive-header.php
+++ template-parts/archive/archive-header.php
@@ -11,9 +11,29 @@
 
 <header class="archive-header-page-header">
 
-	<div class="entry-header-featured-image">
-		<img src="https://s3.wp.wsu.edu/uploads/sites/192/2019/10/brady-bellini-_hpk_92Crhs-unsplash.jpg" alt="white clouds above silhouette of clouds at day">
-	</div>
+	<!-- Featured Image Header -->
+
+	<?php
+	$tax_object = get_queried_object();
+
+	if ($tax_object->taxonomy == "wsuwp_uc_topics") : ?>
+
+		<?php $pod = pods('wsuwp_uc_topics', $tax_object->term_id); ?>
+		<?php $featured_image_display = $pod->display( 'featured_image' ); ?>
+		<?php $featured_image_field = $pod->field( 'featured_image' ); ?>
+		<div class="entry-header-featured-image">
+			<img src="<?php echo $featured_image_display; ?>" alt="<?php echo $featured_image_field['post_title']; ?>">
+		</div>
+
+	<?php else : ?>
+
+		<div class="entry-header-featured-image">
+			<img src="https://s3.wp.wsu.edu/uploads/sites/192/2019/10/brady-bellini-_hpk_92Crhs-unsplash.jpg" alt="white clouds above silhouette of clouds at day">
+		</div>
+
+	<?php endif; ?>
+
+	<!-- Archive Header Heading -->
 
 	<h1 class="archive-header-heading">
 
